9e3ee5b599e9b18d9a56ecc878e72b87eb9649fb
amontano495 <amontano495@gmail.com>
2018-04-12 16:06:37 -0700

    can now find citation count with cli

--- testing/cloutCounter.py
+++ testing/cloutCounter.py
@@ -1,11 +1,15 @@
 from mrjob.job import MRJob
-
-ID = 'e401'
+import sys
 
 class MRCloutCount(MRJob):
 
+    def configure_options(self):
+        super(MRCloutCount, self).configure_options()
+        self.add_passthrough_option(
+                '--article_id',type='string',default='e401')
+
     def mapper(self, _, line):
-        yield "citations", line.count(ID)
+        yield "citations", line.count(self.options.article_id)
 
     def reducer(self, key, counts):
         yield key, sum(counts)
