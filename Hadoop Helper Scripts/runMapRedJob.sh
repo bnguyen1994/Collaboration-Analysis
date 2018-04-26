8aa5fbdfe5e420d31cfbe7fc058ac7e90e5a6cc9
Brandon Nguyen <nguyen.brandon@nevada.unr.edu>
2018-04-26 16:56:39 -0700

    Update runMapRedJob.sh

--- Hadoop Helper Scripts/runMapRedJob.sh
+++ Hadoop Helper Scripts/runMapRedJob.sh
@@ -6,7 +6,26 @@
 # Splash Screen
 cat splashScreen.txt
 
-# Prompt User
+# Prompt User to list directories
+read -p 'List local directory? [Y/N]: ' -n 1 -r
+if  [[ $REPLY =~ ^[Yy]$ ]]
+then 
+        echo ' '
+        ls
+fi
+echo ' '
+
+
+read -p 'List HDFS directory? [Y/N] ' -n 1 -r
+if  [[ $REPLY =~ ^[Yy]$ ]]
+then 
+        echo ' '
+        echo 'LOADING . . .'
+        hdfs dfs -ls -R /
+fi
+echo ' '
+
+# Prompt User for program files
 read -p 'Mapper Program (py): ' mapperFile
 read -p 'Reducer Program (py): ' reducerFile
 read -p 'Input File (HDFS dir): ' inputFile
@@ -17,25 +36,25 @@ echo ' '
 echo Mapper Program: $mapperFile
 echo Reducer Program: $reducerFile
 echo Input File: hdfs://$inputFile
-echo Output File: hdfs://$outputFile
+echo Output File: hdfs://user/hadoop/$outputFile
 echo ' '
 read -p 'Is this correct? [Y/N]: ' -n 1 -r
 
 # If info is correct, run the script
 if  [[ $REPLY =~ ^[Yy]$ ]]
 then 
-	echo ' '
-	hadoop jar /home/hadoop/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.0.jar -files $mapperFile,$reducerFile -mapper $mapperFile -reducer $reducerFile -input hdfs://$inputFile -output $outputFile
+        echo ' '
+        hadoop jar /home/hadoop/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.0.jar -files $mapperFile,$reducerFile -mapper $mapperFile -reducer $reducerFile -input hdfs://$inputFile -output $outputFile
 
-	# Prompt to grab file from HDFS
-	echo ' '
-	read -p 'Get the file from HDFS? [Y/N]: ' -n 1 -r
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		echo ' '
-		hdfs dfs -get $outputFile
-	fi
+        # Prompt to grab file from HDFS
+        echo ' '
+        read -p 'Get the file from HDFS? [Y/N]: ' -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+                echo ' '
+                hdfs dfs -get $outputFile
+        fi
 fi
 echo ' '
+echo 'END SCRIPT'
 exit 1
-
