e2cc2205672e2084ab48607814c3a646999c9f90
Brandon Nguyen <nguyen.brandon@nevada.unr.edu>
2018-04-26 17:33:05 -0700

    Update loadHDFS.sh

--- Hadoop Helper Scripts/loadHDFS.sh
+++ Hadoop Helper Scripts/loadHDFS.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo 'Creating hadoop user directory. . .'
 hdfs dfs -mkdir -p /user/hadoop
+echo 'Creating dblp-ref directory. . .'
 hdfs dfs -mkdir dblp-ref
+echo 'Loading dblp-ref-*.json files. . .'
 hdfs dfs -put dblp-ref/dblp-ref-*.json dblp-ref
+echo 'Listing hdfs directory. . .'
 hdfs dfs -ls -R /
 
 echo 'END SCRIPT'
