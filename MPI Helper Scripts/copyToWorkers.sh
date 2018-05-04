7de843fb6d9df07f352614f42d486586415c1663
Brandon Nguyen <nguyen.brandon@nevada.unr.edu>
2018-05-04 00:07:44 -0700

    Added CLI reading. Still needs to read multi args.

--- MPI Helper Scripts/copyToWorkers.sh
+++ MPI Helper Scripts/copyToWorkers.sh
@@ -1,19 +1,18 @@
 # Prompt User to list directory
-read -p 'List local directory? [Y/N]: ' -n 1 -r
-if  [[ $REPLY =~ ^[Yy]$ ]]
-then 
-        echo ' '
-        ls
-fi
-echo ' '
 
-# Prompt for files
-read -p 'File(s): ' files
-echo ' '
+# Check for commandline arguement
+if [ -z "$1" ]
+then
+        echo 'ERROR: No commandline arguements'
+        echo ' '
+        echo 'SYNTAX: "./copyToWorkers.sh <file>"'
+        echo ' '
+        echo 'END SCRIPT'
+        exit 0
+fi
 
 # Confirm
-
-echo 'File(s) to copy: ' files
+echo 'File(s) to copy: ' $1
 echo ' '
 read -p 'Is this correct? [Y/N]: ' -n 1 -r
 
@@ -22,8 +21,9 @@ if  [[ $REPLY =~ ^[Yy]$ ]]
 then 
         # Copy Files
         for node in node1 node2 node3 node4 node5; do
-            scp $files $node:/home/mpi/;
+            scp $1 $node:/home/mpi/;
         done
 fi
 echo ' '
 echo 'END SCRIPT'
+exit 1
